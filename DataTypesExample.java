268243284c5e2f6022ed3a4421dd1125d66dab51
DSAKEERTHIKA <nathalakeerthikareddy8@gmail.com>
2025-12-10 15:28:12 +0530

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,31 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        byte b = 10;               
+        short s = 200;        
+        int num = 5000;             
+        long bigNum = 150000L; 
+
+        float f = 5.75f;            
+        double d = 19.99;         
+
+        char letter = 'A';         
+        boolean flag = true;       
+        String name = "Java";       
+        int[] arr = {1, 2, 3};      
+
+        
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + num);
+        System.out.println("Long: " + bigNum);
+
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+
+        System.out.println("Char: " + letter);
+        System.out.println("Boolean: " + flag);
+
+        System.out.println("String: " + name);
+        System.out.println("Array element: " + arr[0]);
+    }
+}
