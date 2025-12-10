3419c2319289cd1014aa5d4b31d26482b500fdf2
DSAKEERTHIKA <nathalakeerthikareddy8@gmail.com>
2025-12-10 15:24:18 +0530

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        int age = 30;
+        double price;
+        price = 19.99;
+        String name = "Alice";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+    }
+}
